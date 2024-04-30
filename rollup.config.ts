5a8db271b2d5ca330b0c51baf45aa69d71f5fdc7
weifanyang <1123642601@qq.com>
2024-04-30 23:30:01 +0800

    feat: 添加构建配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,19 +3,49 @@ import typescript from "@rollup/plugin-typescript";
 import path from "node:path";
 import { fileURLToPath } from "url";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
-import pkg from "./package.json" assert { type: "json" };
+// import pkg from "./package.json" assert { type: "json" };
 
-const input = "./src/index.ts";
-const outDir = "dist";
+// const externalDeps = Object.keys(pkg.dependencies || {});
 
-const externalDeps = Object.keys(pkg.dependencies || {});
-
-function resolve(filePath: string) {
+const resolve = (filePath: string) => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   return path.resolve(__dirname, filePath);
-}
+};
+
+const input = "index.ts";
+const output = "index.js";
+const dtsOutput = "index.d.ts";
+const outDir = "dist";
+
+const packagePath = "./packages";
+const corePath = `${packagePath}/core`;
+const corePathInput = `${corePath}/${input}`;
+const requestPath = `${packagePath}/request`;
+const requestPathInput = `${requestPath}/${input}`;
+
+const esmRollupConfig = [
+  {
+    input: resolve(corePathInput),
+    output: resolve(`${corePath}/${outDir}/${output}`),
+  },
+  {
+    input: resolve(requestPathInput),
+    output: resolve(`${requestPath}/${outDir}/${output}`),
+  },
+];
+
+const dtsRollupConfig = [
+  {
+    input: resolve(corePathInput),
+    output: resolve(`${corePath}/${outDir}/${dtsOutput}`),
+  },
+  {
+    input: resolve(requestPathInput),
+    output: resolve(`${requestPath}/${outDir}/${dtsOutput}`),
+  },
+];
 
 const getPlugins = () => {
   return [
@@ -25,31 +55,35 @@ const getPlugins = () => {
   ];
 };
 
-const esmConfig = defineConfig({
-  input: input,
-  external: externalDeps,
-  output: [
-    {
-      file: resolve(`${outDir}/index.js`),
+const esmConfigs = esmRollupConfig.map((config) =>
+  defineConfig({
+    input: config.input,
+    // external: externalDeps,
+    output: [
+      {
+        file: config.output,
+        format: "esm",
+      },
+    ],
+    plugins: getPlugins().concat([
+      terser({
+        output: {
+          ascii_only: true,
+        },
+      }),
+    ]),
+  })
+);
+
+const dtsConfigs = dtsRollupConfig.map((config) =>
+  defineConfig({
+    input: config.input,
+    output: {
+      file: config.output,
       format: "esm",
     },
-  ],
-  plugins: getPlugins().concat([
-    terser({
-      output: {
-        ascii_only: true,
-      },
-    }),
-  ]),
-});
+    plugins: getPlugins().concat([dts()]),
+  })
+);
 
-const dtsConfig = defineConfig({
-  input: input,
-  output: {
-    file: resolve(`${outDir}/index.d.js`),
-    format: "esm",
-  },
-  plugins: getPlugins().concat([dts()]),
-});
-
-export default [esmConfig, dtsConfig];
+export default [...esmConfigs, ...dtsConfigs];
