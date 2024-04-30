8fd699cd9c4002edce1cb278e47f4c5dde327aa4
weifanyang <1123642601@qq.com>
2024-04-30 23:07:58 +0800

    fix: 修复路径报错bug

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -3,4 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "import.meta.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+  },
 });
